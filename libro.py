c9c28cf57709b6623b6b333bc255a079c9942abb
Filippo Di Michele <dimichelefilippo06@gmail.com>
2026-01-29 12:21:39 +0100

    aggiungo commenti

--- libro.py
+++ libro.py
@@ -2,18 +2,18 @@ class Libro:
     def __init__(self,titolo, isbn, autore):
         self.titolo = titolo
         self.isbn = isbn
-        self.disponibile = True
+        self.disponibile = True #Se disponibile
         
         
     def _isdisponibile(self, disponibile):
         return self._isdisponibile
     
     def prestito(self):
-        self.disponibile = False
+        self.disponibile = False #il prestito non è disponibile
         
     def restituisci(self):
-        self.disponibile = True
+        self.disponibile = True  # True se restituisce il prestito
         
         
     def __str__(self):
-        stato = "Disponibile" if self.disponibile else "In prestito"
+        stato = "Disponibile" if self.disponibile else "In prestito" #Solo se disponibile, altrimenti in prestito
