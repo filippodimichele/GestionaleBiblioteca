18e678fe912388c9351b48bc790c2f36b8fb4eed
Filippo Di Michele <dimichelefilippo06@gmail.com>
2026-01-29 12:15:59 +0100

    classe quasi completata + commento

--- utente.py
+++ utente.py
@@ -3,3 +3,13 @@ class Utente:
         self.nome = nome
         self.numero_tessera = numero_tessera
         self.prestiti = []
+        
+    def aggiungi_prestiti(self, prestiti):
+        self.prestiti = prestiti
+        
+    def storico_prestiti(self):
+        return self.prestiti
+    
+    #max 3 prestiti
+    def __str__(self):
+        return f"{self.nome} (Tessera {self.numero_tessera})"
