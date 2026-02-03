4db6dab9ad2d3385215eb66d4545292a46e26117
Filippo <la-tua-email@esempio.com>
2026-02-03 16:48:02 +0100

    modifica vecchia versione di autore

--- autore.py
+++ autore.py
@@ -3,6 +3,11 @@ class Autore:
     def __init__(self, nome, nazionalita):
         self.nome = nome
         self.nazionalita = nazionalita
-        
-    def __str__(self):
-         return f"{self.nome} ({self.nazionalita})" 
+
+    def __repr__(self):
+        return f"Autore(nome='{self.nome}', nazionalita='{self.nazionalita}')"
+
+    def __eq__(self, other):
+        if not isinstance(other, Autore):
+            return False
+        return self.nome == other.nome and self.nazionalita == other.nazionalita
